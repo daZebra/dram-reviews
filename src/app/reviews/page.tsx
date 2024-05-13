1deb3d8fc6a4ebb127598837e62a661e620c416e
Jeremy Cullen <cheech@MacBook-Pro.local>
2024-05-13 16:49:20 -0400

    Dram Reviews v2.0: Minor release fixes ctd.

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -1,17 +1,30 @@
 import ProductOverviewCard from "@/components/product-overview-card";
-import { getProducts } from "@/lib/get-products";
+import { getProducts, ProductQueryParams } from "@/lib/get-products";
 
 export default async function Reviews({
   searchParams,
 }: {
   searchParams: URLSearchParams;
 }) {
-  // console.log(searchParams);
-  const { products, totalCount } = await getProducts(searchParams);
+  // Convert URLSearchParams to ProductQueryParams
+  const productQueryParams: ProductQueryParams = {};
+
+  if (searchParams.has("casks")) {
+    productQueryParams.casks = searchParams.getAll("casks");
+  }
+  if (searchParams.has("tasteNotes")) {
+    productQueryParams.tasteNotes = searchParams.getAll("tasteNotes");
+  }
+  if (searchParams.has("tags")) {
+    productQueryParams.tags = searchParams.getAll("tags");
+  }
+
+  const { products, totalCount } = await getProducts(productQueryParams);
 
   if (!products) {
     return <div>No products found</div>;
   }
+
   return (
     <div className="flex flex-col items-center text-center my-10">
       <h2 className={` text-2xl text-base-content/50`}>
@@ -30,3 +43,36 @@ export default async function Reviews({
     </div>
   );
 }
+
+// import ProductOverviewCard from "@/components/product-overview-card";
+// import { getProducts } from "@/lib/get-products";
+
+// export default async function Reviews({
+//   searchParams,
+// }: {
+//   searchParams: URLSearchParams;
+// }) {
+//   // console.log(searchParams);
+//   const { products, totalCount } = await getProducts(searchParams);
+
+//   if (!products) {
+//     return <div>No products found</div>;
+//   }
+//   return (
+//     <div className="flex flex-col items-center text-center my-10">
+//       <h2 className={` text-2xl text-base-content/50`}>
+//         {totalCount === 0
+//           ? "No products found, try a different search"
+//           : Object.keys(searchParams).length > 0
+//           ? `${totalCount} whiskies found`
+//           : `Expert reviews for over ${totalCount} whiskies`}
+//       </h2>
+//       <section className="flex flex-col items-center w-full px-8 md:max-w-4xl gap-10 py-10">
+//         {products.map((product) => (
+//           <ProductOverviewCard key={product.productName} product={product} />
+//         ))}
+//       </section>
+//       {/* <button className="btn btn-outline rounded-full">Show more</button> */}
+//     </div>
+//   );
+// }
--- src/lib/get-products.ts
+++ src/lib/get-products.ts
@@ -1,7 +1,7 @@
 import { Prisma, ProductItem } from "@prisma/client";
 import prisma from "./db";
 
-type ProductQueryParams = {
+export type ProductQueryParams = {
   casks?: string[];
   tasteNotes?: string[];
   tags?: string[];
@@ -15,20 +15,16 @@ type ProductsResponse = {
 export async function getProducts(
   queryParams: ProductQueryParams = {}
 ): Promise<ProductsResponse> {
-  // Construct the where condition dynamically based on queryParams input
-  // console.log(queryParams);
   let whereCondition: {
     AND?: Prisma.ProductItemWhereInput[];
   } = {};
-  let arrayFields = ["tasteNotes", "casks", "tags"] as const; // Add any other array fields here
+  let arrayFields = ["tasteNotes", "casks", "tags"] as const;
 
-  // Check if queryParams contain only valid fields
   const isValidQuery = Object.keys(queryParams).every((key) =>
-    arrayFields.includes(key)
+    arrayFields.includes(key as any)
   );
 
   if (!isValidQuery) {
-    // If an invalid field is found, return all products
     return {
       products: await prisma.productItem.findMany({
         orderBy: {
@@ -39,20 +35,11 @@ export async function getProducts(
     };
   }
 
-  // Build the where condition based on the queryParams
   let andCondition: Prisma.ProductItemWhereInput[] = [];
   for (const [key, value] of Object.entries(queryParams)) {
-    if (arrayFields.includes(key as (typeof arrayFields)[number])) {
-      if (Array.isArray(value)) {
-        // Create an AND condition for multiple values
-        const conditions = value.map((v) => ({ [key]: { contains: v } }));
-        andCondition.push(...conditions);
-      } else {
-        // Handle single value condition
-        let whereCondition: {
-          [key: string]: Prisma.ProductItemWhereInput | undefined;
-        } = {};
-      }
+    if (arrayFields.includes(key as any) && Array.isArray(value)) {
+      const conditions = value.map((v) => ({ [key]: { contains: v } }));
+      andCondition.push(...conditions);
     }
   }
 
@@ -60,7 +47,6 @@ export async function getProducts(
     whereCondition.AND = andCondition;
   }
 
-  // Fetch products with the conditional where clause
   const products = await prisma.productItem.findMany({
     where: whereCondition,
     orderBy: {
@@ -68,10 +54,87 @@ export async function getProducts(
     },
   });
 
-  // Count the total number of products in the database with the same condition
   const totalCount = await prisma.productItem.count({
     where: whereCondition,
   });
 
   return { products, totalCount };
 }
+
+// import { Prisma, ProductItem } from "@prisma/client";
+// import prisma from "./db";
+
+// type ProductQueryParams = {
+//   casks?: string[];
+//   tasteNotes?: string[];
+//   tags?: string[];
+// };
+
+// type ProductsResponse = {
+//   products: ProductItem[];
+//   totalCount: number;
+// };
+
+// export async function getProducts(
+//   queryParams: ProductQueryParams = {}
+// ): Promise<ProductsResponse> {
+//   // Construct the where condition dynamically based on queryParams input
+//   // console.log(queryParams);
+//   let whereCondition: {
+//     AND?: Prisma.ProductItemWhereInput[];
+//   } = {};
+//   let arrayFields = ["tasteNotes", "casks", "tags"] as const; // Add any other array fields here
+
+//   // Check if queryParams contain only valid fields
+//   const isValidQuery = Object.keys(queryParams).every((key) =>
+//     arrayFields.includes(key)
+//   );
+
+//   if (!isValidQuery) {
+//     // If an invalid field is found, return all products
+//     return {
+//       products: await prisma.productItem.findMany({
+//         orderBy: {
+//           sentimentScore: "desc",
+//         },
+//       }),
+//       totalCount: await prisma.productItem.count(),
+//     };
+//   }
+
+//   // Build the where condition based on the queryParams
+//   let andCondition: Prisma.ProductItemWhereInput[] = [];
+//   for (const [key, value] of Object.entries(queryParams)) {
+//     if (arrayFields.includes(key as (typeof arrayFields)[number])) {
+//       if (Array.isArray(value)) {
+//         // Create an AND condition for multiple values
+//         const conditions = value.map((v) => ({ [key]: { contains: v } }));
+//         andCondition.push(...conditions);
+//       } else {
+//         // Handle single value condition
+//         let whereCondition: {
+//           [key: string]: Prisma.ProductItemWhereInput | undefined;
+//         } = {};
+//       }
+//     }
+//   }
+
+//   if (andCondition.length > 0) {
+//     whereCondition.AND = andCondition;
+//   }
+
+//   // Fetch products with the conditional where clause
+//   const products = await prisma.productItem.findMany({
+//     where: whereCondition,
+//     orderBy: {
+//       sentimentScore: "desc",
+//     },
+//   });
+
+//   // Count the total number of products in the database with the same condition
+//   const totalCount = await prisma.productItem.count({
+//     where: whereCondition,
+//   });
+
+//   return { products, totalCount };
+// }
